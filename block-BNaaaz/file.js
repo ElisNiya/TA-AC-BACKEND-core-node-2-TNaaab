bca38e6630d8f76a5c69a5719e789eedbf099a33
ElisNiya <50265309+ElisNiya@users.noreply.github.com>
2021-12-26 18:36:16 +0100

    Update file.js

--- block-BNaaaz/file.js
+++ block-BNaaaz/file.js
@@ -2,6 +2,7 @@ var http = require('http')
 var server = http.createServer(handleRequest)
 
 function handleRequest(req, res){
+  res.setHeader('content-type', 'text/plain')
   fs.createReadStream('./readme.txt').pipe(res)
 }
 
